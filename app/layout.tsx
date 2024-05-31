dd2f03992f39d5216ebf0a9e6cc8dae45f74d8c5
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-31 13:31:05 +0630

    update(): shadcn ui setup added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,36 @@
+import RootProviders from "@/components/providers/RootProviders";
+import { Toaster } from "@/components/ui/sonner";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-import {
-  ClerkProvider
-} from '@clerk/nextjs'
-import './globals.css'
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Budget Tracker",
+  description: "CodeWithKliton",
+};
+
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body>
-          {children}
+      <html
+        lang="en"
+        className="dark"
+        style={{
+          colorScheme: "dark",
+        }}
+      >
+        <body className={inter.className}>
+          <Toaster richColors position="bottom-right" />
+          <RootProviders>{children}</RootProviders>
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
